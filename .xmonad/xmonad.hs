41ba43f2827adcb5704fe79e61e58e869384d592
Nebu Pookins <nebu@nebupookins.net>
2013-11-03 16:49:11 -0500

    * Defines names for workspaces. * Places key apps on default workspaces * sets default terminal to xfce4-terminal * Adds "handleEventHook = docksEventHook" because that makes something render instantly instead of on next event, or something like that. * Fixes indentation

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,16 +5,27 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-main = do
-xmproc <- spawnPipe "/usr/bin/xmobar /home/nebu/.xmobarrc"
-xmonad $ defaultConfig
-  { manageHook = manageDocks <+> manageHook defaultConfig
-  , layoutHook = avoidStruts  $  layoutHook defaultConfig
-  , logHook = dynamicLogWithPP xmobarPP
-    { ppOutput = hPutStrLn xmproc
-    , ppTitle = xmobarColor "blue" "" . shorten 50
-    }
-  } `additionalKeys`
-  [ ((0, xK_Print), spawn "scrot")
-  ]
+myWorkspaces = ["1:coding","2:web","3:social","4:media","5:utils","6:temp","7","8","9"]
+myManageHook = composeAll
+	[ className =? "KeePass2" --> doShift "5:utils"
+	, className =? "Pidgin"   --> doShift "3:social"
+	, className =? "Skype"    --> doShift "3:social"
+	, className =? "Subl3"    --> doShift "1:coding"
+	-- className =? "Chromium" --> doShift "2:web" --problems with save dialog, etc.
+	]
 
+main = do
+	xmproc <- spawnPipe "/usr/bin/xmobar /home/nebu/.xmobarrc"
+	xmonad $ defaultConfig
+		{ manageHook = composeAll [manageDocks, myManageHook, manageHook defaultConfig]
+		, terminal = "xfce4-terminal"
+		, layoutHook = avoidStruts  $  layoutHook defaultConfig
+		, handleEventHook = docksEventHook <+> handleEventHook defaultConfig
+		, logHook = dynamicLogWithPP xmobarPP
+			{ ppOutput = hPutStrLn xmproc
+			, ppTitle = xmobarColor "blue" "" . shorten 50
+			}
+		, workspaces = myWorkspaces
+		} `additionalKeys`
+		[ ((0, xK_Print), spawn "scrot")
+		]
