aa7e32c2ceb0236f28ca7bc7e15e3a1e408d27b4
Nebu Pookins <nebu@nebupookins.net>
2015-07-03 18:32:32 -0700

    Added GridSelect and WindowBringer to XMonad config.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,14 @@
 import XMonad
+import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys) -- Needs the arch linux package xmonad-contrib
+import XMonad.Actions.GridSelect -- probably needs xmonad-contrib?
+import XMonad.Actions.WindowBringer -- probably needs xmonad-contrib?
 import System.IO
+
+baseConfig = desktopConfig
 
 myWorkspaces =
 	[ "1:<icon=/home/nebu/.xmonad/cpu.xbm/>" --coding
@@ -26,16 +31,20 @@ myManageHook = composeAll
 
 main = do
 	xmproc <- spawnPipe "/usr/bin/xmobar /home/nebu/.xmobarrc"
-	xmonad $ defaultConfig
-		{ manageHook = composeAll [manageDocks, myManageHook, manageHook defaultConfig]
+	xmonad $ baseConfig
+	-- xmonad $ desktopConfig
+		{ manageHook = composeAll [manageDocks, myManageHook, manageHook baseConfig]
 		, terminal = "urxvt"
-		, layoutHook = avoidStruts  $  layoutHook defaultConfig
-		, handleEventHook = docksEventHook <+> handleEventHook defaultConfig
+		, layoutHook = avoidStruts $ layoutHook baseConfig
+		, handleEventHook = docksEventHook <+> handleEventHook baseConfig
 		, logHook = dynamicLogWithPP xmobarPP
 			{ ppOutput = hPutStrLn xmproc
-			, ppTitle = xmobarColor "lightblue" "" . shorten 40
+			, ppTitle = xmobarColor "lightblue" "" . shorten 80
 			}
 		, workspaces = myWorkspaces
 		} `additionalKeys`
-		[ ((0, xK_Print), spawn "scrot")
+		[ ((0, xK_Print), spawn "scrot") -- take screenshot; assumes scot is installed
+		, ((mod1Mask .|. shiftMask, xK_g), goToSelected defaultGSConfig) -- show list of open windows and switch to selected one; provided by XMonad.Actions.GridSelect
+		, ((mod1Mask, xK_g), gotoMenu) -- shows a dmenu of windows, and switches to the workspace containing the selected one; provided by XMonad.Actions.WindowBringer
+		, ((mod1Mask, xK_b), bringMenu) -- shows a dmenu of windows, and brings the selected one to this workspace; provided by XMonad.Actions.WindowBringer
 		]
