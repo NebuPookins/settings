3ac22a934ccc07fb96348edf76849056264bbc29
Nebu Pookins <nebupookins@gmail.com>
2022-03-16 13:03:52 -0700

    Switch from dmenu to rofi; Fix save dialog to appear in center of screen.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,11 +3,15 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops -- For rofi compatibility
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers -- for doRectFloat
+import XMonad.Layout.Spacing
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys) -- Needs the arch linux package xmonad-contrib
 import XMonad.Actions.GridSelect -- probably needs xmonad-contrib?
 import XMonad.Actions.WindowBringer -- probably needs xmonad-contrib?
 import System.IO
+
+import qualified XMonad.StackSet as W -- for RationalRect
 
 padR :: Int -> String -> String
 padR n s
@@ -28,17 +32,18 @@ myWorkspaces =
 	, "9"
 	]
 myManageHook = composeAll
-	[ className =? "KeePass2" --> doShift "5:<icon=/home/nebu/.xmonad/arch.xbm/>"
-	, className =? "Pidgin"   --> doShift "3:<icon=/home/nebu/.xmonad/pacman.xbm/>"
-	, className =? "Skype"    --> doShift "3:<icon=/home/nebu/.xmonad/pacman.xbm/>"
+	[ className =? "KeePass2"        --> doShift "6:util"
+	, className =? "Pidgin"          --> doShift "3:soci"
+	, className =? "Skype"           --> doShift "3:soci"
+	, role =? "GtkFileChooserDialog" --> doRectFloat (W.RationalRect 0.25 0.25 0.5 0.5)
 	-- , className =? "Subl3"    --> doShift "1:<icon=/home/nebu/.xmonad/cpu.xbm/>" --problems with save dialog, etc.
 	-- className =? "Chromium" --> doShift "2:<icon=/home/nebu/.xmonad/fs_01.xbm/>" --problems with save dialog, etc.
 	]
+	where role = stringProperty "WM_WINDOW_ROLE"
 
 main = do
 	xmproc <- spawnPipe "/usr/bin/xmobar /home/nebu/.xmobarrc"
 	xmonad $ ewmh baseConfig
-	-- xmonad $ desktopConfig
 		{ manageHook = composeAll [manageDocks, myManageHook, manageHook baseConfig]
 		, terminal = "xfce4-terminal"
 		, layoutHook = avoidStruts $ layoutHook baseConfig
@@ -50,8 +55,6 @@ main = do
 		, workspaces = myWorkspaces
 		} `additionalKeys`
 		[ ((0, xK_Print), spawn "scrot") -- take screenshot; assumes scot is installed
-		, ((mod4Mask, xK_Tab), spawn "rofi -show combi")
-		, ((mod1Mask .|. shiftMask, xK_g), goToSelected defaultGSConfig) -- show list of open windows and switch to selected one; provided by XMonad.Actions.GridSelect
-		, ((mod1Mask, xK_g), gotoMenu) -- shows a dmenu of windows, and switches to the workspace containing the selected one; provided by XMonad.Actions.WindowBringer
-		, ((mod1Mask, xK_b), bringMenu) -- shows a dmenu of windows, and brings the selected one to this workspace; provided by XMonad.Actions.WindowBringer
+		, ((mod4Mask .|. shiftMask, xK_s), spawn "flameshot gui") -- take screenshot; assumes flameshot is installed.
+		, ((mod1Mask, xK_p), spawn "rofi -show combi")
 		]
