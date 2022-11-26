75861b9019fb61d72bc0f4f7cbb86cf5cad9a83e
Nebu Pookins <nebupookins@gmail.com>
2022-11-26 14:02:26 -0800

    random-wallpaper: use bg-fill instead of bg-max
    This works better if the aspect ratio of the background doesn't match
    the screen. The tradeoff is that you may have content cut off from the
    background image, but at least you don't get black borders.

--- bin/random-wallpaper.sh
+++ bin/random-wallpaper.sh
@@ -4,7 +4,7 @@ WALLPAPER_DIR="/home/nebu/h/My Documents/My Pictures/Wallpapers/"
 
 while true; do
 	if [ -d "$WALLPAPER_DIR" ]; then
-		feh --bg-max --randomize "$WALLPAPER_DIR"
+		feh --bg-fill --randomize "$WALLPAPER_DIR"
 		sleep 15m
 	else
 		echo "$WALLPAPER_DIR does not seem to be a directory. Exiting."
